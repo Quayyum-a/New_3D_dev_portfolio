7e5773afd022a79573e86f70d981c9b8a59390b6
Quayyum <quayyumariyo@gmail.com>
2025-08-19 11:56:25 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ const App = () => {
     <div className="container mx-auto max-w-7xl">
       <Navbar />
       <Hero />
+      <section className='min-h-screen'/>
+      <section className='min-h-screen'/>
+      <section className='min-h-screen'/>
+      <section className='min-h-screen'/>
+      <section className='min-h-screen'/>
+      <section className='min-h-screen'/>
     </div>
   )
 }
