c6df4dc6aaf227d45cb5ce11fed7677307d95a9c
Quayyum <quayyumariyo@gmail.com>
2025-08-19 15:47:09 +0100

    Update Astronaut.jsx and Hero.jsx with new 3D astronaut and hero section improvements

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,15 +1,29 @@
-import HeroText from "../components/HeroText"
-import ParallaxBackground from "../components/ParallaxBackground"
+import { Canvas } from "@react-three/fiber";
+import HeroText from "../components/HeroText";
+import ParallaxBackground from "../components/ParallaxBackground";
+import { Astronaut } from "../components/Astronaut";
+import { OrbitControls } from "@react-three/drei";
+import { useMediaQuery } from "react-responsive";
 const Hero = () => {
+  const isMobile = useMediaQuery({ maxWidth: 853 });
   return (
     <section className="flex items-start justify-center md:items-center md:justify-start min-h-screen overflow-hidden c-space">
       <HeroText />
-      <ParallaxBackground/>
-      <figure className="absolute">
-
+      <ParallaxBackground />
+      <figure
+        className="absolute inset-0"
+        style={{ width: "100vw", height: "100vh" }}
+      >
+        <Canvas camera={{ position: [0, 1, 3] }}>
+          <Astronaut
+            scale={isMobile ? 0.23 : 0.3}
+            position={isMobile ? [0, -1.5, 0] : [2.3, -1, 0]}
+          />
+          <OrbitControls />
+        </Canvas>
       </figure>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
