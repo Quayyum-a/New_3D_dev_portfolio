4740773f004b8877c6a6514f3f01a5324b7bf5b7
Quayyum <quayyumariyo@gmail.com>
2025-08-20 10:54:23 +0100

    chore: update About section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,3 +1,4 @@
+import Card from "../components/Card";
 
 const About = () => {
   return (
@@ -26,8 +27,28 @@ const About = () => {
         <div className="grid-default-color grid-2">
           <div className="flex items-center justify-center w-full h-full">
             <p className="flex items-end text-5xl text-gray-500">
-              CODE IS CRAFT
+              SOFTWARE DEVELOPMENT IS A CRAFT
             </p>
+            <Card
+              style={{ rotate: "75deg", top: "30%", left: "20%" }}
+              text="GRASP"
+            />
+            <Card
+              style={{ rotate: "-30deg", top: "60%", left: "45%" }}
+              text="SOLID"
+            />
+            <Card
+              style={{ rotate: "90deg", bottom: "30%", left: "70%" }}
+              text="Design Patterns"
+            />
+            <Card
+              style={{ rotate: "-45deg", top: "55%", left: "0%" }}
+              text="Design Principles"
+            />
+            <Card
+              style={{ rotate: "20deg", top: "10%", left: "38%" }}
+              text="SRP"
+            />
           </div>
         </div>
         {/* Grid 3 */}
