cdd974ac0cf6a46334098a123c516415f319ca4a
Quayyum <quayyumariyo@gmail.com>
2025-08-23 10:12:24 +0100

    updated files

---
+++ src/components/Project.jsx
@@ -0,0 +1,14 @@
+
+const Project = () => {
+  return (
+    <div className="flex-wrap items-center justify-between py-10 space-y-14 sm:flex sm:space-y-0">
+      <p className="text-2xl">title</p>
+      <div>
+        <span>tag1</span>
+        <span>tag1</span>
+      </div>
+    </div>
+  );
+}
+
+export default Project
